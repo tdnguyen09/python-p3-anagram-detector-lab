ebf9abdeaae4716d49ea4160127b81a62db0cfb1
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-05-01 18:17:57 +1000

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self, provided_list):
+        anagram_list = list()
+        word_list = sorted([letter for letter in self.word])
+        for anagram_word in provided_list:
+            if sorted([letter for letter in anagram_word]) == word_list:
+                anagram_list.append(anagram_word)
+        return anagram_list 
